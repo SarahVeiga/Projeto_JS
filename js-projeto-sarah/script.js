8dea99636540377b62f6baf309f4ddf77787680d
SarahVeiga <sarahveiga55@gmail.com>
2025-03-25 10:45:55 -0300

    Atividade Projeto JS

--- js-projeto-sarah/script.js
+++ js-projeto-sarah/script.js
@@ -1,1 +1,41 @@
-// Digitação (JS Completo)
+// Controle do menu mobile
+const menuIcon = document.querySelector('#menu-icon');
+const navList = document.querySelector('.navlist');
+
+menuIcon.addEventListener('click',() => {
+    menuIcon.classList.toggle('bx-x');
+    navList.classList.toogle('open');
+
+    // Bloquear o scroll quando o menu estiver aberto
+    document.body.style.overflow = navList.classList.contains('open') ? 'hidden' : 'auto';
+});
+
+// Fechar o menu ao clicar nos links
+document.querySelectorAll('.navlist a').forEach(link => {
+    link.addEventListener('click', () => {
+        menuIcon.classList.remove('bx-x');
+        navList.classList.remove('open');
+        document.body.style.overflow = 'auto';
+    });
+});
+
+// Fechar ao rolar a página
+window.addEventListener('scroll', () => {
+    if(navList.classList.contains('open')){
+        menuIcon.classList.remove('bx-x');
+        navList.classList.remove('open');
+        document.body.style.overflow = 'auto';
+    }
+});
+
+// ==== Navegação ativa ====
+// Seleciona todos os links de navegação
+const navLinks = document.querySelectorAll('.navlist a');
+
+// Função para adicionar a classe 'active' no link clicado
+function activeLink() {
+    navLinks.forEach(item => item.classList.remove('active'));
+    this.classList.add('active');
+}
+// Adiciona um evento de clique no link de navegação
+navLinks.forEach(item => item.addEventListener('click', activeLink));
